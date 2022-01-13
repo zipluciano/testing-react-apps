6f96f6ddffb6e2cab0f14b74c4cd5b03ed2f8de2
Luciano E. Montiero <luciano.es.mo@gmail.com>
2022-01-13 17:58:17 -0300

    Ex 01

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -1,29 +1,65 @@
 // simple test with ReactDOM
 // http://localhost:3000/counter
 
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import Counter from '../../components/counter'
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Counter from "../../components/counter";
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use ReactDOM.render to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 increment.click())
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 decrement.click())
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
-})
+afterEach(() => (document.body.innerHTML = ""));
+
+test("counter increments and decrements when the buttons are clicked", () => {
+  const div = document.createElement("div");
+  document.body.append(div);
+  ReactDOM.render(<Counter />, div);
+
+  const [decrement, increment] = div.querySelectorAll("button");
+  const message = div.firstChild.querySelector("div");
+
+  expect(message.textContent).toBe("Current count: 0");
+
+  increment.click();
+  expect(message.textContent).toBe("Current count: 1");
+
+  decrement.click();
+  expect(message.textContent).toBe("Current count: 0");
+});
+
+test("counter increments and decrements when the buttons are clicked", () => {
+  const div = document.createElement("div");
+  document.body.append(div);
+  ReactDOM.render(<Counter />, div);
+
+  const [decrement, increment] = div.querySelectorAll("button");
+  const message = div.firstChild.querySelector("div");
+
+  expect(message.textContent).toBe("Current count: 0");
+
+  increment.click();
+  expect(message.textContent).toBe("Current count: 1");
+
+  decrement.click();
+  expect(message.textContent).toBe("Current count: 0");
+});
+
+// describe("counter increments and decrements when the buttons are clicked", () => {
+//   it("inside of it declaration", () => {
+//     const div = document.createElement("div");
+//     document.body.append(div);
+//     ReactDOM.render(<Counter />, div);
+
+//     const [decrement, increment] = div.querySelectorAll("button");
+//     const message = div.firstChild.querySelector("div");
+
+//     expect(message.textContent).toBe("Current count: 0");
+
+//     increment.click();
+//     expect(message.textContent).toBe("Current count: 1");
+
+//     decrement.click();
+//     expect(message.textContent).toBe("Current count: 0");
+
+//     div.remove();
+//   });
+// });
 
 /* eslint no-unused-vars:0 */
